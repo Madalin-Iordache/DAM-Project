83759009a4922ed21bc79ee158a847ed959c3b81
Madalin-Iordache <m.iordache28@gmail.com>
2020-11-10 17:48:20 +0200

    Changed mapping on controller

--- src/main/java/com/feaa/project/GreetingController.java
+++ src/main/java/com/feaa/project/GreetingController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class GreetingController {
 
-    @GetMapping("/greeting")
+    @GetMapping("/")
     public String greeting(@RequestParam(name="name", required=false, defaultValue="China") String name, Model model) {
         model.addAttribute("name", name);
         return "greeting";
